196d8a2b4c486d5499f36b078ef657026069698d
LiveIsAbsurd <anndreybykov19@gmail.com>
2023-09-25 15:50:03 +0300

    ll

--- src/index.js
+++ src/index.js
@@ -1,7 +1,33 @@
 import React from 'react';
-import ReactDOM from 'react-dom/client';
-
+import ReactDOM from 'react-dom';
+import { Provider } from 'react-redux';
+import { configureStore, createReducer } from '@reduxjs/toolkit';
 import App from './components/app/app';
 
+const inState = {
+    filter: 'ALL'
+}
+
+const filterReducer = (state = inState.filter, action) => {
+    switch (action.type) {
+        case 'SWITCH_FILTER':
+            return 'ALL';
+        default:
+            return state;
+    }
+};
+
+const rootReducer = createReducer({
+    filter: filterReducer
+});
+
+let store = configureStore({
+    reducer: rootReducer
+});
+
 const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(<App />);
+root.render(
+  <Provider store={store.getState()}>
+    <App />
+  </Provider>
+);
